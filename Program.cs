7ecee5c79b479fe1a64cb7d09830ed1478f8596c
SanjeshKumarKabilan <sanjeshkumar684@gmail.com>
2022-03-02 17:36:00 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delecates
+{
+    internal class Program
+    {
+        delegate void demoDelegate(string x, int n);
+
+        public static void PrintPersonalDetails(string xName, int xAge)
+        {
+            Console.WriteLine($"Name {xName}");
+            Console.WriteLine($"Age  {xAge}");
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            demoDelegate dd;
+
+            dd = new demoDelegate(PrintPersonalDetails);
+            dd("Deepeck", 20);
+
+
+            Console.ReadKey();
+        }
+    }
+}
